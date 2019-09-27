f865890955511842744b209dc4547d8fb98453ba
samuelhklumpers <samuelhklumpers@gmail.com>
2019-09-27 16:12:46 +0200

    implement some demos

---
+++ demo.py
@@ -0,0 +1,14 @@
+from tests import Create4444, Create666, AnimateTile, ShowAnimate
+#Exp2_6_1, Exp2_6_2, Exp2_6_3, Exp2_6_4, Exp2_6_5
+
+#print("Exploratory questions")
+#print("2.6.1")
+#Exp2_6_1()
+##Exp2_6_2()
+#Exp2_6_3()
+#Exp2_6_4()
+#print("2.6.5")
+#Exp2_6_5()
+ani1 = ShowAnimate(40, 2.3, 100, 10)
+ani2 = AnimateTile(Create666(4))
+ani3 = AnimateTile(Create4444(4))
